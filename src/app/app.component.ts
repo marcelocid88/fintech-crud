0acf99637b0b3175e78ed678198eb2e0f4f5db42
ftfernandes <ftfernandes@gmail.com>
2026-09-07 17:22:03 -0300

    intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./shared/menu/menu.component";
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fintechcrud';
+  titulo = 'Fintech CRUD';
+  nome = 'Ana';
 }

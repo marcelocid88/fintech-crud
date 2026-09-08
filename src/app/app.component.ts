fc0a2058a48d852eb16efaaf9bce2a077c1c2ec3
MARCELO CID TADEU GARCIA <marcelo.ctgarcia@senacsp.edu.br>
2026-09-08 20:43:49 -0300

    'Atualizando'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./shared/menu/menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fintechcrud';
+  titulo:string = 'fintechcrud';
+  nome:string ='MarceloCid';
 }
